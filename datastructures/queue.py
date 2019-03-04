f080298dbf0615a7902c5771559d7c8ff1f6c3ed
Lenny Boyatzis <lennyboyatzis@gmail.com>
2019-03-04 19:41:38 +1100

    extended queue and stack implementations

--- datastructures/queue.py
+++ datastructures/queue.py
@@ -2,16 +2,28 @@ from typing import Any
 
 
 class Queue():
+    """
+    First come first served (FIFO)
+    """
     def __init(self):
         self.store = []
 
     def is_empty(self):
+        """
+        Checks whether the stack is empty
+        """
         return True if not self.store else False
 
-    def push(self, val: Any) -> None:
+    def enqueue(self, val: Any) -> None:
+        """
+        Adds a value to the queue
+        """
         self.store.append(val)
 
-    def pop(self) -> Any:
+    def dequeue(self) -> Any:
+        """
+        Returns and removes the LEAST recently added key
+        """
         if self.is_empty():
             raise Exception('Unable to pop, queue is empty')
         return self.store.pop(0)
--- datastructures/stack.py
+++ datastructures/stack.py
@@ -2,16 +2,36 @@ from typing import Any
 
 
 class Stack():
+    """
+    Last in first out (LIFO)
+    """
     def __init__(self):
         self.store = []
 
     def is_empty(self):
+        """
+        Checks whether the stack is empty
+        """
         return True if not self.store else False
 
     def push(self, val: Any) -> None:
+        """
+        Adds a value to the top of the stack
+        """
         self.store.append(val)
 
+    def top(self) -> Any:
+        """
+        Returns the top value on the stack
+        """
+        if self.is_empty():
+            raise Exception('Unable to top, stack is empty')
+        self.store[-1]
+
     def pop(self) -> Any:
+        """
+        Returns and removes the MOST recently added key
+        """
         if self.is_empty():
             raise Exception('Unable to pop, stack is empty')
         return self.store.pop()
