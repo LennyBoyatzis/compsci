0fef38fb973fd05bea28f551d6a6d3b2251d7831
Lenny Boyatzis <lennyboyatzis@gmail.com>
2019-07-03 09:23:09 +1000

    reverse words first pass

--- problems/reverse_words.py
+++ problems/reverse_words.py
@@ -10,4 +10,23 @@ def reverse_words(message: List) -> List:
     Returns:
         Reversed words in a list
     """
-    pass
+    left = 0
+    right = len(message) - 1
+
+    while left < right:
+        message[left], message[right] = message[right], message[left]
+        left += 1
+        right -= 1
+
+    return message
+
+
+if __name__ == "__main__":
+    message = ['c', 'a', 'k', 'e', ' ',
+               'p', 'o', 'u', 'n', 'd', ' ',
+               's', 't', 'e', 'a', 'l']
+
+    print("message", message)
+
+    result = reverse_words(message)
+    print("result", result)
